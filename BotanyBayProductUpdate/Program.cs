880aada0cc88976e4c2216fca3ec4d973fff801d
Elliot Morris <3lliot1337@gmail.com>
2018-01-24 12:44:37 +0000

    email added

--- BotanyBayProductUpdate/Program.cs
+++ BotanyBayProductUpdate/Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using cwbx;
 using System.Data.Odbc;
 using System.IO;
-using CsvHelper;
+using System.Net.Mail;
+
 namespace BotanyBayProductUpdate
 {
     class Program
-    {
-             
+    {             
         static void Main(string[] args)
         {
-            string result = string.Empty;
+            //properties for sending mail
+            MailMessage mail = new MailMessage("elliot.morris@wsg.co.uk", "elliotmorris115@gmail.com");
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "192.20.20.11";            
+            Attachment fileCsv;
+            //File path location
+            var fileName = "BotBayUpdate" + DateTime.Now.ToString("dd-MM-yyyy-hh-mm-ss") + ".csv";
+            var filePath = @"C:\Users\morrise\Desktop\";
 
+            string result = string.Empty;
             StringConverter stringConverter = new StringConverterClass();
 
             //Define as400 system and connect
@@ -33,14 +43,12 @@ namespace BotanyBayProductUpdate
                 program.LibraryName = "SILIB";
                 program.ProgramName = "BOTBAY01";
                 program.system = system;
-
-
+                
                 //Finally call the program
-
                 try
                 {
                     // program.Call();
-
+                    //creating a odbc connection to the as400
                     using (OdbcConnection conn = new OdbcConnection("Driver={iseries access odbc driver};system=s654d1bb;uid=PCS400;pwd=PCS400;"))
                     {
                         OdbcCommand command = new OdbcCommand("select * from silib.botbay01up", conn);
@@ -48,45 +56,67 @@ namespace BotanyBayProductUpdate
                         OdbcDataReader reader = command.ExecuteReader();
 
                         var csv = new StringBuilder();
+                        //header string
                         var header = string.Format("Barcode, Price, Description, Dept");
-                        var filePath = @"C:\Users\morrise\Desktop\BotBay.csv";
-                        csv.AppendLine(header);
+                        //appending the headers
+                        csv.AppendLine(header);                                  
 
                         while (reader.Read())
-                        {
-                            
+                        {                            
                             Console.WriteLine("Barcode = {0} Price = {0} Description = {0} Dept = {0} ", reader[0],  reader[1], reader[2], reader[3]);
+                            //reading in the values into the csv
                             var first = reader[0].ToString();
                             var second = reader[1].ToString();
                             var third = reader[2].ToString();
                             var fourth = reader[3].ToString();
-                            var newLine = string.Format("{0}, {1}, {2}, {3}", first, second, third, fourth);                            
+                            //creating the string format
+                            var newLine = string.Format("{0}, {1}, {2}, {3}", first, second, third, fourth);
+                            //appending the values
                             csv.AppendLine(newLine);
                         }
-                        File.WriteAllText(filePath, csv.ToString());
-                    }    
-                }
-                catch (Exception ex)
-                {
-                    if (system.Errors.Count > 0)
-                    {
-                        foreach (cwbx.Error error in system.Errors)
-                        {
-                            Console.WriteLine(error.Text);
-                        }
-                    }
 
-                    if (program.Errors.Count > 0)
+                          File.WriteAllText(filePath + fileName, csv.ToString());                            
+                    }                   
+                 }
+                    catch (Exception ex)
                     {
-                        foreach (cwbx.Error error in program.Errors)
+                        if (system.Errors.Count > 0)
                         {
-                            Console.WriteLine(error.Text);
+                            foreach (cwbx.Error error in system.Errors)
+                            {
+                                Console.WriteLine(error.Text);
+                            }
                         }
-                    }
-                } 
-            }
-            system.Disconnect(cwbcoServiceEnum.cwbcoServiceAll);
-            Console.ReadKey();
+
+                        if (program.Errors.Count > 0)
+                        {
+                            foreach (cwbx.Error error in program.Errors)
+                            {
+                                Console.WriteLine(error.Text);
+                            }
+                        }
+                    } 
+                }
+                if (filePath.Length >= 1)
+                {
+                    fileCsv = new Attachment(filePath + fileName);
+                    mail.Subject = "BotBay product update ";
+                    mail.Body = "Hi, Please see the csv file attached. ";
+                    mail.Attachments.Add(fileCsv);
+                    client.Send(mail);
+                    Console.WriteLine("Mail sent");
+                }
+                else
+                {
+                    mail.Subject = "BotBay product update (No updates)";
+                    mail.Body = "There are no new upates today. ";
+                    client.Send(mail);
+                    Console.WriteLine("Csv is empty");
+                }
+
+                //closing connection
+                system.Disconnect(cwbcoServiceEnum.cwbcoServiceAll);
+                Console.ReadKey();
         }             
      }
 }
